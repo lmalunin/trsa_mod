cafe279383e59c2e212429a56b2ad2a3474090ad
Malunin Lev <lmalunin@gmail.com>
2025-04-15 14:20:51 +0300

    03.02

--- toolkit/tools.go
+++ toolkit/tools.go
@@ -1,3 +1,20 @@
 package toolkit
 
+import "crypto/rand"
+
+const randomStringSource = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_+"
+
 type Tools struct{}
+
+func (t *Tools) RandomString(n int) string {
+	var s, r []rune
+	s = make([]rune, n)
+	r = []rune(randomStringSource)
+	for i := range s {
+		p, _ := rand.Prime(rand.Reader, len(r))
+		x := p.Uint64()
+		y := uint64(len(r))
+		s[i] = r[x%y]
+	}
+	return string(s)
+}
