c470c96df8aea965d31834ccd13f71895886bc19
Malunin Lev <lmalunin@gmail.com>
2025-04-16 16:58:33 +0300

    04.07

--- toolkit/tools_test.go
+++ toolkit/tools_test.go
@@ -123,3 +123,78 @@ func TestTools_UploadFiles(t *testing.T) {
 		wg.Wait()
 	}
 }
+
+func TestTools_UploadOneFile(t *testing.T) {
+
+	// Очищаем директорию перед тестом
+
+	if err := os.RemoveAll("./testdata/upload-one"); err != nil {
+		t.Fatal(err)
+	}
+
+	// Создаем директорию для загрузки
+	if err := os.MkdirAll("./testdata/upload-one", 0755); err != nil {
+		t.Fatal(err)
+	}
+
+	for _, e := range uploadTests {
+		pr, pw := io.Pipe()
+		writer := multipart.NewWriter(pw)
+
+		go func() {
+			defer writer.Close()
+
+			file, err := os.Open("./testdata/img.png")
+			if err != nil {
+				t.Error(err)
+				return
+			}
+			defer file.Close()
+
+			part, err := writer.CreateFormFile("file", "./testdata/img.png")
+			if err != nil {
+				t.Error(err)
+				return
+			}
+
+			_, err = io.Copy(part, file)
+			if err != nil {
+				t.Error(err)
+				return
+			}
+		}()
+
+		request := httptest.NewRequest("POST", "/", pr)
+		request.Header.Add("Content-Type", writer.FormDataContentType())
+
+		var testTools Tools
+
+		uploadedFile, err := testTools.UploadOneFile(request, "./testdata/upload-one", true)
+		if err != nil {
+			t.Error(err)
+		}
+
+		if !e.errorExpected {
+			t.Logf("Uploaded file: %+v", uploadedFile)
+			filePath := fmt.Sprintf("./testdata/upload-one/%s", uploadedFile.NewFileName)
+			t.Logf("Checking file at: %s", filePath)
+
+			if _, err := os.Stat(filePath); os.IsNotExist(err) {
+				t.Errorf("%s: Expected file to exist: %s", e.name, uploadedFile.NewFileName)
+			} else {
+				t.Logf("File exists at: %s", filePath)
+			}
+
+			src := filePath
+			dst := fmt.Sprintf("./testdata/upload-one/keep_%s", uploadedFile.NewFileName)
+			t.Logf("Moving file from %s to %s", src, dst)
+			if err := os.Rename(src, dst); err != nil {
+				t.Errorf("Error moving file: %v", err)
+			}
+		}
+
+		if !e.errorExpected && err != nil {
+			t.Errorf("%s: error expected but none received", e.name)
+		}
+	}
+}
